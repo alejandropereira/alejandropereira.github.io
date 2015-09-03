f6e290bbf285280d8a89770790a832fa8e29d43c
Alejandro Pereira <ja.pereiragt@gmail.com>
2015-09-03 14:59:28 -0600

    Blog update at 2015-09-03 20:59:27

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+$(function() {
+  var menu;
+  menu = $('#menu');
+  $('.hero__title').fitText(0.9, {
+    minFontSize: '55px',
+    maxFontSize: '90px'
+  });
+  $('#toggle-menu').on('click', function(e) {
+    e.preventDefault();
+    return menu.slideToggle(300);
+  });
+  return $(window).on('resize', function() {
+    if ($(window).width() > 720) {
+      return menu.show();
+    } else {
+      return menu.hide();
+    }
+  });
+});
